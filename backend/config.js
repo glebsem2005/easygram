049ebfd9c7e43d6b5675f170b58a5898a8107711
Gleb Semenov <144787082+glebsem2005@users.noreply.github.com>
2025-07-14 16:38:49 +0300

    Create config.js

---
+++ backend/config.js
@@ -0,0 +1,7 @@
+module.exports = {
+  WS_PORT: 8080,
+  HTTP_PORT: 3000,
+  MEDIA_DIR: 'backend/uploads',
+  MAX_FILE_SIZE_MB: 20,
+  ALLOWED_MIME_TYPES: ['image/png', 'image/jpeg', 'video/mp4', 'audio/mpeg'],
+};
