4b5453fdb1652d0343ef318b116e7089a78ef6f4
Gleb Semenov <144787082+glebsem2005@users.noreply.github.com>
2025-07-14 18:24:34 +0300

    Update AuthScreen.js

--- frontend/screens/AuthScreen.js
+++ frontend/screens/AuthScreen.js
@@ -3,7 +3,7 @@ import { View, TextInput, Button, Text, StyleSheet, Alert } from 'react-native';
 import axios from 'axios';
 import { AuthContext } from '../App';
 
-const BACKEND_URL = 'http://localhost:3000'; // поменяй на свой адрес
+const BACKEND_URL = 'http://localhost:8080'; // поменяй на свой адрес
 
 export default function AuthScreen() {
   const [isLogin, setIsLogin] = useState(true);
